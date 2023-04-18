efb95788af9d36b7bebbe1c75583f6659d403f8f
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-04-19 01:10:33 +0600

    inisal setup

---
+++ src/components/SingUp/SingUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SingUp.css'
+
+const SingUp = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SingUp;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKZlCpxiDOQDH_WwwpLSG-3YAG91DEDVA",
+  authDomain: "ema-jhon-auth-a2606.firebaseapp.com",
+  projectId: "ema-jhon-auth-a2606",
+  storageBucket: "ema-jhon-auth-a2606.appspot.com",
+  messagingSenderId: "895380256797",
+  appId: "1:895380256797:web:c7dc1fa854fe1a7b1ceef4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
